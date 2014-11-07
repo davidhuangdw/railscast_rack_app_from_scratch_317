85736b89474e37f9430cf05ea34925e7ec832fc2
DavidHdw <tossboydavid@sina.com>
2014-11-07 17:52:31 +0800

    haml pass scope/locals

--- config.ru
+++ config.ru
@@ -1,5 +1,14 @@
 require 'greeter'
 
 use Rack::Reloader,0
+# use Rack::File.new('public')
+
+# run Greeter.new
+
+use Rack::Auth::Basic  do |user, password|
+  password == 'secret'
+end
 
 run Greeter.new
+# run Rack::Cascade.new([Rack::File.new('public'), Greeter.new])
+# run Rack::File.new('public')
--- lib/greeter.rb
+++ lib/greeter.rb
@@ -2,25 +2,32 @@ require 'haml'
 gem 'rack'
 
 class Greeter
+  attr_reader :request
   def call(env)
-    request = Rack::Request.new(env)
+    @request = Rack::Request.new(env)
     case request.path
       when '/'
         response render('index')
       when '/change'
-        response request.params['name']
+        response request.params['name'] do |res|
+          res.set_cookie("greet", request.params['name'])
+          res.redirect("/")
+        end
       else
         response "Not Found", 404
     end
   end
 
-  def render(template)
+  def render(template, **locals)
     path = File.expand_path("../views/#{template}.haml", __FILE__)
-    Haml::Engine.new(File.read(path)).render
+    Haml::Engine.new(File.read(path)).render(self)
   end
 
-  def response(*args)
-    Rack::Response.new(*args)
+  def response(*args, &blk)
+    Rack::Response.new(*args, &blk)
+  end
+  def name
+    request.cookies["greet"] || "Guest"
   end
 end
 
